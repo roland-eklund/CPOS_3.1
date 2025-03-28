c76501c619831b9fc0d62d64b26c369b6c79dcd9
roland-eklund <roleklun@gmail.com>
2025-03-28 13:50:39 +0200

    philosophers with monitor, done

--- dining_philosophers/dining_philosophers.c
+++ dining_philosophers/dining_philosophers.c
@@ -19,12 +19,14 @@
 #define NUM_PHILOSOPHERS 5 // threads
 #define ROUNDS 800
 #define MONITOR_INTERVAL 40      // in microseconds
-#define TIME_UNTIL_DEAD 10000000 // in microseconds
+#define TIME_UNTIL_DEAD 10000000  // in microseconds
+#define SIMULATE_WORK 1 // 1 = TRUE, 0 = FALSE
 
 pthread_mutex_t forks[NUM_PHILOSOPHERS];
 pthread_mutex_t data_lock[NUM_PHILOSOPHERS];
 pthread_t philosophers[NUM_PHILOSOPHERS];
 pthread_t monitor;
+//pthread_barrier_t start_barrier;
 
 struct PhilData { // Philosopher struct to track data
   int phil_id;
@@ -38,6 +40,8 @@ struct PhilData phil_data[NUM_PHILOSOPHERS]; // Array for phil structs
 
 atomic_int monitor_running = 1;
 
+// Monitor checks if a thread is starved, based on treshold (TIME_UNTIL_DEAD)
+// If so, monitor sets the state to dead
 void *data_monitor() {
   while (monitor_running) {
     usleep(MONITOR_INTERVAL);
@@ -60,56 +64,95 @@ void *data_monitor() {
 }
 
 void *philosopher(void *arg) {
-  struct PhilData *data = (struct PhilData *)arg;
+//  pthread_barrier_wait(&start_barrier);
+  int phil_id = *(int *)arg;
+  free(arg);
 
-  while (data->rounds < ROUNDS && !(data->is_dead)) {
-    // usleep(100);
-    //  printf("Philosopher %d is waiting..", data->phil_id);
-    clock_gettime(CLOCK_MONOTONIC, &data->start_time);
-    if ((data->phil_id % 2) == 0) {
+  // Loops until total cycles equals set constant or until thread is starved.
+  while (1) {
+
+    pthread_mutex_lock(&data_lock[phil_id]);
+    if (phil_data[phil_id].rounds >= ROUNDS || phil_data[phil_id].is_dead) {
+      pthread_mutex_unlock(&data_lock[phil_id]);
+      break;
+    }
+    clock_gettime(CLOCK_MONOTONIC, &phil_data[phil_id].start_time);
+    pthread_mutex_unlock(&data_lock[phil_id]);
+
+    if ((phil_id % 2) == 0) {
       // Grab left fork first
-      pthread_mutex_lock(&forks[data->phil_id]);
-      pthread_mutex_lock(&forks[(data->phil_id + 1) % NUM_PHILOSOPHERS]);
+      pthread_mutex_lock(&forks[phil_id]);
+      pthread_mutex_lock(&forks[(phil_id + 1) % NUM_PHILOSOPHERS]);
     } else {
       // Grab right fork first
-      pthread_mutex_lock(&forks[(data->phil_id + 1) % NUM_PHILOSOPHERS]);
-      pthread_mutex_lock(&forks[data->phil_id]);
+      pthread_mutex_lock(&forks[(phil_id + 1) % NUM_PHILOSOPHERS]);
+      pthread_mutex_lock(&forks[phil_id]);
     }
-    clock_gettime(CLOCK_MONOTONIC, &data->end_time);
-    // printf("philosopher %d is eating", id);
-    // Drop fork (left)
-    pthread_mutex_unlock(&forks[data->phil_id]);
-    pthread_mutex_unlock(&forks[(data->phil_id + 1) % NUM_PHILOSOPHERS]);
+    if (SIMULATE_WORK){
+    int sleep_time = rand() % 201 + 10;
+    usleep(sleep_time);
+    }
 
-    pthread_mutex_lock(&data_lock[data->phil_id]);
-    data->rounds++;
-    pthread_mutex_unlock(&data_lock[data->phil_id]);
+    pthread_mutex_unlock(&forks[phil_id]);
+    pthread_mutex_unlock(&forks[(phil_id + 1) % NUM_PHILOSOPHERS]);
+
+    pthread_mutex_lock(&data_lock[phil_id]);
+    clock_gettime(CLOCK_MONOTONIC, &phil_data[phil_id].end_time);
+    phil_data[phil_id].rounds++;
+    pthread_mutex_unlock(&data_lock[phil_id]);
   }
   return NULL;
 }
 
 int main() {
-  struct timespec prog_start_time;
-  struct timespec prog_end_time;
-
+  struct timespec prog_start_time, prog_end_time;
   clock_gettime(CLOCK_MONOTONIC, &prog_start_time);
+  // pthread_mutex_t init_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+  // initializing mutexes
+  for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+    pthread_mutex_init(&forks[i], NULL);     // initializing forks (mutexes)
+    pthread_mutex_init(&data_lock[i], NULL); // mutexes for data
+  }
+
+  // initializing phils data
+ // pthread_barrier_init(&start_barrier, NULL, NUM_PHILOSOPHERS + 1);
+
+  // Creating the philosopher threads
 
   for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-    pthread_mutex_init(&forks[i], NULL); // initializing forks (mutexes)
-    pthread_mutex_init(&data_lock[i], NULL);
-    pthread_create(&philosophers[i], NULL, philosopher, &phil_data[i]);
+    pthread_mutex_lock(&data_lock[i]);
     phil_data[i].phil_id = i;
     phil_data[i].rounds = 0;
     phil_data[i].is_dead = 0;
+    pthread_mutex_unlock(&data_lock[i]);
+    int *id = malloc(sizeof(int));
+    *id = i;
+    pthread_create(&philosophers[i], NULL, philosopher, id);
   }
 
+  //pthread_barrier_wait(&start_barrier); // All philosophers wait here before starting
+
+  // Creating the monitor thread
   pthread_create(&monitor, NULL, data_monitor, NULL);
 
+  // Waiting for phil threads to finnish
   for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
     pthread_join(philosophers[i], NULL);
   }
-  monitor_running = 0;
+
+  // Finnish running the monitor
+  atomic_store(&monitor_running, 0);
   pthread_join(monitor, NULL);
+
+  // Destroying mutexes
+  for (int i = 0; i <= NUM_PHILOSOPHERS; i++) {
+    pthread_mutex_destroy(&data_lock[i]);
+    pthread_mutex_destroy(&forks[i]);
+  }
+  // Destroying start_barrier
+  //pthread_barrier_destroy(&start_barrier);
+
   clock_gettime(CLOCK_MONOTONIC, &prog_end_time);
 
   // :::: STATS ::::: //
